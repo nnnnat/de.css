8ba0968f415ccf9d858d75599732b3404f453746
Nat Hamilton <info@nathamilton.com>
2018-01-04 18:16:00 -0600

    updated to the newest build tool, updated code font-size

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -1,19 +1,2 @@
-const variables = require('./postcss.variables.js')
-const mixins = require('./postcss.mixins.js')
-const media = require('./postcss.media.js')
-const selectors = require('./postcss.selectors.js')
-
-module.exports = {
-  plugins: {
-    'postcss-import': {},
-    'postcss-each': {},
-    'postcss-mixins': { mixins },
-    'postcss-simple-vars': { variables },
-    'postcss-nested': {},
-    'postcss-custom-selectors': { ...selectors },
-    'postcss-custom-media': { ...media },
-    'postcss-font-family-system-ui': {},
-    'postcss-color-function': {},
-    'autoprefixer': {}
-  }
-}
+const config = require('de.css.config')
+module.exports = { ...config }
--- config/postcss.selectors.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  'extensions': {
-    ':--enter': ':hover, :focus',
-    ':--button': 'button, [type=submit], [type=reset], [type=button]',
-    ':--fields': 'input[type=text], input[type=email], input[type=password], input[type=search], input[type=number], input[type=tel], input[type=date], select, textarea'
-  }
-}
